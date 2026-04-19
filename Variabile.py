f9ed2f1d0ffca979da7b3599e82241edb9883359
Emi900 <117076940+Emi900@users.noreply.github.com>
2026-04-19 21:32:21 +0300

    Variabile

---
+++ Variabile.py
@@ -0,0 +1,52 @@
+#Aici o sa fie primea mea aplicatie in Python
+print("Vreau sa invat Python")
+print ("Cat mai repede")
+
+#Variabile - string, integer, float, boolean
+
+
+# Aici avem Strings
+first_name = "Emi"
+food = "sarmale"
+email = "emi@gmail.com"
+print(first_name)
+print(f"Hello {first_name}")
+print(f"Imi place {food}")
+print(f"E-mail-ul tau este : {email}")
+
+#Integers 
+age = 23
+cantitate = 3
+numar_studenti = 10
+print(f"Tu ai {age} de ani")
+print (f"Am cumparat {cantitate}kg de banane")
+print(f"In clasa 12B sunt {numar_studenti} studenti")
+
+#Float
+pret = 10.99
+distanta = 5.5
+print(f"Pretul este de ${pret}")
+print(f"Am alergat {distanta} km in 4 ore")
+
+#Boolean 
+
+is_student = True
+#print(f"Tu esti student?{is_student}")
+if is_student:
+    print("Tu esti student")
+else:
+    print("Tu nu esti student")
+
+# _______________Tema______________
+
+nume = "Benedic Emanuel" #string
+print(f"Numele meu este {nume}")
+varsta = 23 #integer
+print(f"Am {varsta} ani")
+inaltime = 1.74 #float
+print(f"Am o inlatime de {inaltime}")
+e_student = False
+if e_student:
+    print("Tu esti student")
+else:
+    print("Tu nu este student")
