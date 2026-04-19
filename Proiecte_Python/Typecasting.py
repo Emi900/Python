171014a369245e7bb5f7d3eab9fc2b8ad215c47b
Emi900 <117076940+Emi900@users.noreply.github.com>
2026-04-19 21:55:03 +0300

    py

---
+++ Proiecte_Python/Typecasting.py
@@ -0,0 +1,11 @@
+#Typecasting = str(), int(), float(), bool() practic conversie 
+
+
+nume = "Benedic Emanuel" #string
+varsta = 23 #integer
+inaltime = 1.74 #float
+e_student = False
+
+varsta = str(varsta)
+
+print(type(varsta))
---
+++ Proiecte_Python/input.py
@@ -0,0 +1,11 @@
+nume = input("Care este numele tau?: ")
+age = int(input("Cati ani ai?: "))
+
+age = age + 1
+
+print(f"Hello {nume}!")
+print(f"Tu ai {age} ani!")
+
+
+
+#Ex 1
