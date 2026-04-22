090a1d8197d97e81ee214fc110b31ed7f886e688
Emi <emanuelbenedic61@gmail.com>
2026-04-22 20:02:18 +0300

    Ex

---
+++ Proiecte_Python/Teorema lui Pitagora.py
@@ -0,0 +1,14 @@
+# Teorema lui Pitagora 
+
+# ip²= c² + c²
+# BC² = AB² + AC²
+
+import math
+
+
+c1 = float(input("Introdu cateta 1 : "))
+c2 = float(input("Introdu cateta 2 : "))
+
+ip = math.sqrt(pow(c1, 2)+ pow(c2, 2))
+
+print(f"Ipotenuza este {ip}")
